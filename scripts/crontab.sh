264c026248f78252b3de8aaa2db630b03eb79d5c
Tim Patzelt <patzelt.tim@gmail.com>
2025-12-05 21:23:30 +0000

    dedicated autorestic script

--- scripts/crontab.sh
+++
@@ -1,4 +0,0 @@
-# This is a copy of the crontab commands (as root)
-PATH="/usr/local/bin:/usr/bin:/bin"
-
-0 3 * * * (date && autorestic -c /home/tim/coding/homelab/.autorestic.yml backup -a) >> /var/log/autorestic.log 2>&1
