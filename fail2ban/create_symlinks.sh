c1e03d8a960d0980f517ab0b361763ea0a8557ed
Tim Patzelt <23495178+tpatzelt@users.noreply.github.com>
2026-03-19 09:48:01 +0000

    Add fail2ban

---
+++ fail2ban/create_symlinks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo ln -s /home/tim/coding/homelab/fail2ban/caddy-json.conf /etc/fail2ban/filter.d/caddy-json.confln -s /home/tim/coding/homelab/fail2ban/caddy.local /etc/fail2ban/jail.d/caddy.local
